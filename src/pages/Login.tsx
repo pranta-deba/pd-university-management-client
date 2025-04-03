ee03af2d215a76ad3d4dae26e054b4f852d4f7a6
Pranto Deb <rajprit0m@outlook.com>
2025-04-04 01:35:58 +0600

    [Auth]: ---> [Login]: ---> set cookies in the browser

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,54 @@
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0002",
+      password: "admin1234",
+    },
+  });
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log("Data ==>", data);
+  console.log("Error ==>", error);
+
+  const onSubmit = (data: { userId: string; password: string }) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+
+    login(userInfo);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="userId">ID: </label>
+        <input
+          className=""
+          type="text"
+          id="userId"
+          placeholder="ID"
+          {...register("userId")}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input
+          className=""
+          type="text"
+          id="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
