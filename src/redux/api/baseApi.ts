a1bf35cb962012ff30be57450a6916d590979acd
Pranto Deb <rajprit0m@outlook.com>
2025-04-04 21:41:37 +0600

    [Admin]: ---> [Academic Semester]: ---> Setting up baseQuery to send accessToken with each request"

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include", // for cookies
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("Authorization", token);
+    }
+    return headers;
+  },
+});
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    credentials: "include", // for cookies
-  }),
+  baseQuery: baseQuery,
   endpoints: () => ({}),
 });
